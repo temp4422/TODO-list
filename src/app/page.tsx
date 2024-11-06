df46a521ba1f017b0787617cf4edaffeb8ef8b07
user <user@MacBook-Pro-user.local>
2024-11-06 14:46:00 +0200

    Fix spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,8 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
+// prettier-ignore
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form'
 
 export default function Home() {
   const formSchema = z.object({
@@ -81,8 +73,10 @@ export default function Home() {
         <main className="">
           <Card>
             <CardHeader>
-              <CardDescription className="flex justify-end relative">
-                <p className="absolute left-0 text-accent">Домашні справи</p>
+              <CardDescription className="flex justify-end relative mb-2">
+                <p className="absolute left-0 text-accent text-xs font-bold border-2 p-1 border-accent">
+                  домашні справи
+                </p>
                 <button>
                   <svg
                     width="24"
@@ -120,7 +114,7 @@ export default function Home() {
                   </svg>
                 </button>
               </CardDescription>
-              <CardTitle>Купити продукти</CardTitle>
+              <CardTitle className="text-xl">Купити продукти</CardTitle>
             </CardHeader>
             <CardContent>
               <p>Скласти список необхідних продуктів та відвідати магазин.</p>
@@ -144,9 +138,7 @@ export default function Home() {
             </CardFooter>
           </Card>
 
-          <hr className="bg-[#999999] h-[2px] m-2" />
-
-          <div className="flex justify-end">
+          <div className="flex justify-end my-2">
             <Button className="bg-accent">
               <svg
                 width="14"
@@ -161,90 +153,94 @@ export default function Home() {
             </Button>
           </div>
 
-          <hr className="bg-[#999999] h-[2px] m-2" />
+          <div className="">
+            <hr className="bg-[#999999] h-[2px] m-2" />
+            <h2>Категорія</h2>
+            <p className="text-xs flex">
+              <p className="text-accent">домашні справи &nbsp; </p> робота фітнес
+            </p>
 
-          <h2>Категорія</h2>
-          <p className="text-xs">домашні справи робота фітнес</p>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="mt-6">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Заголовок</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} className="bg-white" />
+                      </FormControl>
+                      {/* <FormDescription>This is your public display name.</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Заголовок</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" {...field} className="bg-white" />
-                    </FormControl>
-                    {/* <FormDescription>This is your public display name.</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem className="mt-4">
+                      <FormLabel>Примітка</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} className="bg-white h-28" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Примітка</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" {...field} className="bg-white h-28" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Buttons cancel/save */}
-              <div className="flex justify-end gap-1">
-                <Button className="bg-red-500" type="submit">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g clipPath="url(#clip0_3_465)">
-                      <path
-                        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
-                        fill="#333333"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_3_465">
-                        <rect width="24" height="24" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  Скасувати
-                </Button>
-                <Button className="bg-accent" type="submit">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g clipPath="url(#clip0_3_460)">
-                      <path
-                        d="M8.99997 16.17L4.82997 12L3.40997 13.41L8.99997 19L21 7.00003L19.59 5.59003L8.99997 16.17Z"
-                        fill="white"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_3_460">
-                        <rect width="24" height="24" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  Зберегти
-                </Button>
-              </div>
-            </form>
-          </Form>
+                {/* Buttons cancel/save */}
+                <div className="flex justify-end gap-1 mt-6">
+                  <Button className="bg-red-500" type="submit">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_3_465)">
+                        <path
+                          d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                          fill="#333333"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_3_465">
+                          <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    Скасувати
+                  </Button>
+                  <Button className="bg-accent" type="submit">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_3_460)">
+                        <path
+                          d="M8.99997 16.17L4.82997 12L3.40997 13.41L8.99997 19L21 7.00003L19.59 5.59003L8.99997 16.17Z"
+                          fill="white"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_3_460">
+                          <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    Зберегти
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
         </main>
 
         <footer></footer>
