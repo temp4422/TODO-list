319eab3246d01b39cfdfe62d70a331fd9622c90c
user <user@MacBook-Pro-user.local>
2024-11-06 12:02:41 +0200

    Add shadcn card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+// prettier-ignore
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function Home() {
   return (
@@ -8,13 +10,39 @@ export default function Home() {
         <header>
           <h1 className="text-4xl wheight-bold">Список справ</h1>
           <Input className="bg-white" />
-          <p>list1 list2 list2 </p>
+          <p>category1 category2 category2 </p>
           <hr className="bg-[#999999] h-[2px] m-2" />
           <br />
         </header>
 
         <main>
-          list-item1 list-item 2 list-item3
+          <Card>
+            <CardHeader>
+              <CardDescription>Домашні справи</CardDescription>
+              <CardTitle>Купити продукти</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Скласти список необхідних продуктів та відвідати магазин.</p>
+            </CardContent>
+            <CardFooter>
+              <Button className="bg-[#FF6600]">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M14.59 5.58L8 12.17L4.41 8.59L3 10L8 15L16 7L14.59 5.58ZM10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 18C5.58 18 2 14.42 2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18Z"
+                    fill="white"
+                  />
+                </svg>
+                Завершити
+              </Button>
+            </CardFooter>
+          </Card>
+
           <hr className="bg-[#999999] h-[2px] m-2" />
           <div className="flex justify-end">
             <Button className="bg-[#FF6600]">
