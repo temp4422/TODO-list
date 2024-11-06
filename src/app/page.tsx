87180c9fc57fc03c8829a605618073f7f78beb0d
user <user@MacBook-Pro-user.local>
2024-11-06 14:18:44 +0200

    Add svg to card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,45 @@ export default function Home() {
         <main className="">
           <Card>
             <CardHeader>
-              <CardDescription>Домашні справи</CardDescription>
+              <CardDescription className="flex justify-end relative">
+                <p className="absolute left-0 ">Домашні справи</p>
+                <button>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clip-path="url(#clip0_2054_47)">
+                      <path
+                        d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04Z"
+                        fill="#333333"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_2054_47">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </button>
+
+                <button>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM19 4H15.5L14.5 3H9.5L8.5 4H5V6H19V4Z"
+                      fill="#333333"
+                    />
+                  </svg>
+                </button>
+              </CardDescription>
               <CardTitle>Купити продукти</CardTitle>
             </CardHeader>
             <CardContent>
