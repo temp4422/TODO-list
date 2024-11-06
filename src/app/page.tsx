a01759f8599184ded57f8f799a76789f58f937f7
user <user@MacBook-Pro-user.local>
2024-11-06 12:34:27 +0200

    WIP form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,15 +103,17 @@ export default function Home() {
           <hr className="bg-[#999999] h-[2px] m-2" />
 
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
+              <h2>Категорія</h2>
+              <p className="text-xs">Домашні справи робота фітнес</p>
               <FormField
                 control={form.control}
                 name="username"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Username</FormLabel>
+                    <FormLabel>Заголовок</FormLabel>
                     <FormControl>
-                      <Input placeholder="shadcn" {...field} />
+                      <Input placeholder="shadcn" {...field} className="bg-white" />
                     </FormControl>
                     <FormDescription>This is your public display name.</FormDescription>
                     <FormMessage />
@@ -119,28 +121,53 @@ export default function Home() {
                 )}
               />
 
-              <Button className="bg-[#FF6600]" type="submit">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_3_460)">
-                    <path
-                      d="M8.99997 16.17L4.82997 12L3.40997 13.41L8.99997 19L21 7.00003L19.59 5.59003L8.99997 16.17Z"
-                      fill="white"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_3_460">
-                      <rect width="24" height="24" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-                Зберегти
-              </Button>
+              {/* Buttons cancel/save */}
+              <div className="flex justify-end gap-1">
+                <Button className="bg-red-500" type="submit">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_3_465)">
+                      <path
+                        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                        fill="#333333"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_3_465">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  Скасувати
+                </Button>
+                <Button className="bg-[#FF6600]" type="submit">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_3_460)">
+                      <path
+                        d="M8.99997 16.17L4.82997 12L3.40997 13.41L8.99997 19L21 7.00003L19.59 5.59003L8.99997 16.17Z"
+                        fill="white"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_3_460">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  Зберегти
+                </Button>
+              </div>
             </form>
           </Form>
         </main>
