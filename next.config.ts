7695153f7ef7981012317401b3c54860aefb7547
user <user@MacBook-Pro-user.local>
2024-11-09 13:58:38 +0200

    Change distDir

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // Enable a static export for static sites
   output: 'export',
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'docs',
+  // distDir: 'docs',
 
   // Deploy a Next.js application under a sub-path of a domain
   // basePath: '/docs',
